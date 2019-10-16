82a5496aee638346da61b2d501bb7775ec407ff4
Fanil Zubairov <fannisco@gmail.com>
2019-10-16 14:58:27 +0200

    Panel border

--- src/ui/components/Panel.tsx
+++ src/ui/components/Panel.tsx
@@ -13,7 +13,7 @@ export const Panel: React.FC<IProps> = (props) => {
     const Panel = styled.div`
         margin: ${sizes.spacing}px;
         padding: ${sizes.elementPadding}px;
-        border: 1px solid ${colors.baseL1};
+        border: 1px solid ${colors.baseL3};
         border-radius: ${sizes.borderRadius}px;
         color: ${colors.text};
         font: ${sizes.fontSize}px/${Fonts.lh} '${Fonts.ff}';
