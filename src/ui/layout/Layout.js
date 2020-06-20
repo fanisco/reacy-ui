bb046bdd9a362157f5f961e0e8ae7d8ede567be0
Fanil Zubairov <fannisco@gmail.com>
2020-06-20 21:09:06 +0200

    Layout

--- src/ui/layout/Layout.js
+++ src/ui/layout/Layout.js
@@ -2,7 +2,15 @@ import React from 'react';
 import './Layout.scss';
 
 const Layout = ({children}) => {
-  return <div className="layout">{children}</div>
+  return (
+    <div className="layout">
+      <header className="layout__header"></header>
+      <main className="layout__content">
+        <div className="layout__container">{children}</div>
+      </main>
+      <footer className="layout__footer"></footer>
+    </div>
+  )
 }
 
 export default Layout;
