0effda2cb452861c546c3101d382e0d683007617
fa.zubairov <fa.zubairov@tensor.ru>
2020-06-26 12:50:49 +0200

    App components'

--- src/ui/app/Amount.js
+++ src/ui/app/Amount.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import Input from '../kit/form/Input';
 import Button from '../kit/buttons/Button';
+import {faPlus, faMinus} from '@fortawesome/free-solid-svg-icons';
 import './Amount.scss';
 
 const Amount = ({amount, onChange}) => {
+  const increment = e => e.preventDefault() || onChange(amount + 1);
+  const decrement = e => e.preventDefault() || onChange(amount - 1);
   return (
     <div className="amount">
-      <Button caption="-" onClick={e => {
-        e.preventDefault();
-        onChange(amount - 1);
-      }}/>
-      <input type="text" value={amount} onChange={e => onChange(e.target.value)}/>
-      <Button caption="+" onClick={e => {
-        e.preventDefault();
-        onChange(amount + 1);
-      }}/>
+      <Button
+        icon={faMinus}
+        mods={['gray', 'narrow', 'stacked-right']}
+        onClick={decrement}
+      />
+      <Input
+        type="text"
+        value={amount}
+        mods={['narrow', 'text-center', 'roundness-none', 'eased']}
+        onChange={e => onChange(e.target.value)}
+      />
+      <Button
+        icon={faPlus}
+        mods={['gray', 'narrow', 'stacked-left']}
+        onClick={increment}
+      />
     </div>
   )
 }
