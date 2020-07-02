46a350332f907b75ba88fee42302de02741805c1
Fanil Zubairov <fannisco@gmail.com>
2020-07-02 12:50:40 +0200

    Exo vars

--- src/core/helpers.ts
+++ src/core/helpers.ts
@@ -28,32 +28,32 @@ export const getColors = (style: number = -1): {
 } => {
   if (style === -1) {
     return {
-      foreground: colors.b550,
-      foregroundHover: colors.b900,
-      foregroundActive: colors.b900,
-      background: colors.b100,
-      backgroundHover: colors.b250,
-      backgroundActive: colors.b200,
-      border: colors.b300,
-      borderHover: colors.b400,
-      borderActive: colors.b300,
+      foreground: colors.d500,
+      foregroundHover: colors.d500,
+      foregroundActive: colors.d500,
+      background: colors.l100,
+      backgroundHover: colors.l200,
+      backgroundActive: colors.l200,
+      border: colors.l500,
+      borderHover: colors.l500,
+      borderActive: colors.l500,
       link: palette[0].a400,
-      linkHover: palette[0].a450,
+      linkHover: palette[0].a500,
       linkActive: palette[0].a500
     };
   } else {
     return {
-      foreground: colors.b100,
-      foregroundHover: colors.b100,
-      foregroundActive: colors.b100,
-      background: palette[style].a300,
-      backgroundHover: palette[style].a350,
-      backgroundActive: palette[style].a400,
+      foreground: colors.l100,
+      foregroundHover: colors.l100,
+      foregroundActive: colors.l100,
+      background: palette[style].a500,
+      backgroundHover: palette[style].a400,
+      backgroundActive: palette[style].a600,
       border: palette[style].a400,
-      borderHover: palette[style].a450,
+      borderHover: palette[style].a500,
       borderActive: palette[style].a500,
       link: palette[style].a400,
-      linkHover: palette[style].a450,
+      linkHover: palette[style].a500,
       linkActive: palette[style].a500
     }
   }
