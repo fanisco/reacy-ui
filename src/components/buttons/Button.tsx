26b2758c7f23701b378835f8fce7d056ade33d78
Fanil Zubairov <fannisco@gmail.com>
2020-07-03 17:19:36 +0200

    Index files

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {Buttons} from 'common/interfaces';
 import {bemClasses} from 'common/bem';
 
-export const Button: React.FC<Buttons.IButton> = ({onClick, href, children, mods, disabled = false, ...props}) => {
-  const className = bemClasses('rcy-button', mods, props.className);
+export const Button: React.FC<Buttons.IButton> = ({onClick, href, children, disabled = false, ...props}) => {
+  const className = bemClasses('rcy-button', props.mods, props.className);
   if (href) {
     return <a onClick={onClick} href={href} className={className}>{children}</a>;
   } else {
--- src/components/forms/Input.tsx
+++ src/components/forms/Input.tsx
@@ -1,23 +1,21 @@
-export const a = 1;
-// import React from 'react';
-//
-// export const Input: React.FC<Forms.IInput> = ({value, onChange, placeholder, mods, name, type = 'text', disabled = false}) => {
-//   return (
-//     <_Input mods={modsFromArray(mods)}
-//             type={type}
-//             name={name}
-//             value={value}
-//             disabled={disabled}
-//             placeholder={placeholder}
-//             onChange={e => {
-//               if (onChange) {
-//                 onChange(e.target.value)
-//               }
-//             }}
-//     />
-//   );
-// };
-//
-// const _Input = styled.input<Core.IStyled>`
-//   ${props => input(props.mods)}
-// `;
+import React from 'react';
+import {Forms} from 'common/interfaces';
+import {bemClasses} from 'common/bem';
+
+export const Input: React.FC<Forms.IInput> = ({value, name, onChange, placeholder, type = 'text', disabled = false, ...props}) => {
+  const className = bemClasses('rcy-input', props.mods, props.className);
+  return (
+    <input className={className}
+           type={type}
+           name={name}
+           value={value}
+           disabled={disabled}
+           placeholder={placeholder}
+           onChange={e => {
+             if (onChange) {
+               onChange(e.target.value)
+             }
+           }}
+    />
+  );
+};
