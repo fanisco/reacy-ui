416d1f9b1e726fe5ec3d338a01547cc1a5b04e61
Fanil Zubairov <fannisco@gmail.com>
2020-07-05 22:42:54 +0200

    Cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import {Button} from 'reacy-ui';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className=""
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-        <Button href="https://reactjs.org"
-                className="App-link"
-                onClick={() => void 0}
-                mods={['inline']}
-        >Learn React</Button>
-      </header>
-    </div>
+    <Button href="https://reactjs.org"
+            className=""
+            onClick={() => void 0}
+            mods={['inline']}
+    >Test</Button>
   );
 }
 
