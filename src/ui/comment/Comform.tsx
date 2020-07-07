cab69ff3a59cd2c0166ea93eebb190672e519a99
fa.zubairov <fa.zubairov@tensor.ru>
2020-07-07 09:38:50 +0200

    Posting comment with hotkeys

--- src/ui/comment/Comform.tsx
+++ src/ui/comment/Comform.tsx
@@ -6,27 +6,45 @@ import {addComment} from '../../state/actions';
 
 export const Comform: React.FC<{}> = () => {
   const [comment, setComment] = useState('');
-  const {state, dispatch} = useContext(Context);
+  const {dispatch} = useContext(Context);
+  const [ctrlPressed, setCtrlPressed] = useState(false);
+  const submitComment = async () => {
+    if (!comment) {
+      return;
+    }
+    // Create a new comment
+    await addComment({
+      dispatch,
+      text: comment,
+      authorId: 1,
+      parentId: 0,
+      date: (new Date()).getTime()
+    });
+    // Empty the textbox
+    setComment('');
+  };
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Control') {
+      setCtrlPressed(true);
+    }
+    if (e.key === 'Enter' && ctrlPressed) {
+      submitComment();
+    }
+  };
+  const onKeyUp = (e: KeyboardEvent) => {
+    if (e.key === 'Control') {
+      setCtrlPressed(false);
+    }
+  };
   return (
     <Wrapper>
       <FormWrapper>
-        <Form fields={[{name: 'comment', type: 'textarea', placeholder: 'Type your comment here...'}]}
+        <Form fields={[{name: 'comment', type: 'textarea', placeholder: 'Type your comment here...', onKeyDown, onKeyUp}]}
               data={{comment}}
               onChange={(name: string, value: string) => setComment(value)}/>
       </FormWrapper>
       <ButtonWrapper>
-        <Button onClick={() => {
-          // Create a new comment
-          addComment({
-            dispatch,
-            text: comment,
-            authorId: 1,
-            parentId: 0,
-            date: (new Date()).getTime()
-          });
-          // Empty the textbox
-          setComment('');
-        }}
+        <Button onClick={submitComment}
                 mods={['outline', 'rounded', 'primary', 'bold']}
                 disabled={!comment.length}
         ><Icon name="reply"/>Send</Button>
