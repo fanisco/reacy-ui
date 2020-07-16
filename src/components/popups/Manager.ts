726955f6b4ca9cdc1b17bea90fad698bdf4d83c5
Fanil Zubairov <fannisco@gmail.com>
2020-07-16 11:16:28 +0200

    Popups container error

--- src/components/popups/Manager.ts
+++ src/components/popups/Manager.ts
@@ -4,6 +4,9 @@ const container: HTMLDivElement = document.getElementById('rcy-popups');
 export const Manager = {
   zIndex: 1,
   render(popup: any): any {
+    if (!container) {
+      throw Error('Container element "#rcy-popups" is not presented in DOM. Unable to create popups.');
+    }
     const style = popup.props.style || {};
     return ReactDOM.createPortal({
       ...popup,
