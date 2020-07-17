0c4d9daa23d89bdddbca934fb2211d57887ccecf
Fanil Zubairov <fannisco@gmail.com>
2020-07-17 12:33:54 +0200

    Tabs

--- src/components/lists/Tabs.tsx
+++ src/components/lists/Tabs.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import {Lists} from '../../common/interfaces';
-import {bemClasses, defaultMods} from '../../common/bem';
-import {StyleMods} from '../../common/mods';
+import {bemClasses, defaultSize} from '../../common/bem';
 import {List} from './List';
 import {Button} from '../buttons';
 
 export const Tabs: React.FC<Lists.ITabs> = ({activeId, items, onClick, ...props}) => {
-  const mods = defaultMods(props.mods);
+  const mods = defaultSize(props.mods);
+  const activityMod = 'active';
   const classBase = 'rcy-tabs';
   const className = bemClasses(classBase, mods, props.className);
-  const activityMod = mods.find(mod => mod in StyleMods) || StyleMods.primary;
-  const itemMods = mods.filter(mod => !(mod in StyleMods));
+  const itemMods = mods.filter(mod => mod !== activityMod);
   return (
     <List className={className} mods={[...mods, 'flex-rows']}>
       {items.map(item => {
-        const mods = [...itemMods, 'underline'].filter(mod => mod !== null);
+        const mods = [...itemMods];
         if (activeId === item.id) {
           mods.push(activityMod);
         }
         return <Button
-          className={`${classBase}__item`}
-          mods={mods}
+          className={bemClasses(`${classBase}__item`, mods)}
           onClick={() => onClick && onClick(item.id)}
         >
           {item.caption}
