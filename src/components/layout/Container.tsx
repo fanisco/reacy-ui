b01a5ab2a9468966b8c79dabfedfcf4603bac410
Fanil Zubairoff <fannisco@gmail.com>
2020-09-03 21:37:55 +0200

    Small fix for layout/Container

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,11 +1,9 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Layout} from '../../common/interfaces';
 import {bemClasses} from '../../common/bem';
 
 export const Container: React.FC<Layout.IContainer> = ({children, ...props}) => {
-  const st = useState(1);
-  console.log(st);
-  const classBase = 'rcy-container123';
+  const classBase = 'rcy-container';
   const className = bemClasses(classBase, props.mods, props.className);
   return (
     <div className={className}>
